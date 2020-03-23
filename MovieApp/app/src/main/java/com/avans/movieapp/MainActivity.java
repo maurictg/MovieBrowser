4329d98907a1c6b9a245824e677b787b1dbf0421
Ramon van Sprundel <ramones156@users.noreply.github.com>
2020-03-23 19:56:43 +0100

    Added navigation and fragments

--- MovieApp/app/src/main/java/com/avans/movieapp/MainActivity.java
+++ MovieApp/app/src/main/java/com/avans/movieapp/MainActivity.java
@@ -1,15 +1,49 @@
 package com.avans.movieapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.avans.movieapp.fragments.HomeFragment;
+import com.avans.movieapp.fragments.ProfileFragment;
+import com.avans.movieapp.fragments.SavedFragment;
+import com.avans.movieapp.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = MainActivity.class.getSimpleName();
+
+    // Bottom navigation buttons
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = item -> {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.nav_search:
+                fragment = new SearchFragment();
+                break;
+            case R.id.nav_saved:
+                fragment = new SavedFragment();
+                break;
+            case R.id.nav_profile:
+                fragment = new ProfileFragment();
+                break;
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+        return true;
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+
     }
 }
