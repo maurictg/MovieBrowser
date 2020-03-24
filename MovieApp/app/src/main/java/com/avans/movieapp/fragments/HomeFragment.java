35b0714a0843914447e4d9d794fee1fffcf39300
MaurICT <31762519+maurictg@users.noreply.github.com>
2020-03-24 11:08:01 +0100

    Implement adapter for Home fragment

--- MovieApp/app/src/main/java/com/avans/movieapp/fragments/HomeFragment.java
+++ MovieApp/app/src/main/java/com/avans/movieapp/fragments/HomeFragment.java
@@ -3,6 +3,7 @@ package com.avans.movieapp.fragments;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,25 +14,46 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.avans.movieapp.MainActivity;
 import com.avans.movieapp.R;
+import com.avans.movieapp.adapters.VideosAdapter;
+import com.avans.movieapp.models.Movie;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+    private final String TAG = HomeFragment.class.getSimpleName();
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
+    private ArrayList<Movie> movies;
+    private View view;
+    private RecyclerView rvHome;
+    private VideosAdapter adapter;
+
+    public HomeFragment() {}
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        Log.d(TAG, "Called onCreateView");
+        movies = new ArrayList<>();
+
         // Inflate the layout for this fragment\
-        View v = inflater.inflate(R.layout.fragment_home, container, false);
-        RecyclerView mHomeRecycler = v.findViewById(R.id.rvHome);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        rvHome = view.findViewById(R.id.rvHome);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity(), MainActivity.calculateNoOfColumns(getActivity()));
-        mHomeRecycler.setLayoutManager(layoutManager);
-        return v;
+        rvHome.setLayoutManager(layoutManager);
+
+        adapter = new VideosAdapter(movies);
+        rvHome.setAdapter(adapter);
+
+        movies.add(new Movie("Test"));
+        movies.add(new Movie("Test 2"));
+        movies.add(new Movie("Testje"));
+        adapter.notifyDataSetChanged();
+
+        return view;
     }
 
 }
