5df5e72b134b3b1830e8192e78dc88ef0cea74d2
AartPieterse <aartpieterse01@gmail.com>
2020-03-25 17:11:46 +0100

    small changes detail activity

--- MovieApp/app/src/main/java/com/avans/movieapp/fragments/MovieDetailsActivity.java
+++ MovieApp/app/src/main/java/com/avans/movieapp/fragments/MovieDetailsActivity.java
@@ -2,21 +2,16 @@ package com.avans.movieapp.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.avans.movieapp.R;
 import com.avans.movieapp.models.Movie;
 import com.squareup.picasso.Picasso;
 
-public class MovieDetailsActivity extends Fragment {
+public class MovieDetailsActivity extends AppCompatActivity {
 
     private final String TAG = MovieDetailsActivity.class.getSimpleName();
 
@@ -30,26 +25,28 @@ public class MovieDetailsActivity extends Fragment {
     private TextView rating;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate called");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.movie_detail);
 
-        View v = inflater.inflate(R.layout.movie_detail, container, false);
+        image = findViewById(R.id.movie_detail_image);
+        title = findViewById(R.id.movie_detail_title);
+        genre = findViewById(R.id.movie_detail_genre);
+        company = findViewById(R.id.movie_detail_company);
+        summary = findViewById(R.id.movie_detail_summary);
+        age = findViewById(R.id.movie_detail_age);
+        rating = findViewById(R.id.movie_detail_rating);
 
-        image = v.findViewById(R.id.movie_detail_image);
-        title = v.findViewById(R.id.movie_detail_title);
-        genre = v.findViewById(R.id.movie_detail_genre);
-        company = v.findViewById(R.id.movie_detail_company);
-        summary = v.findViewById(R.id.movie_detail_summary);
-        age = v.findViewById(R.id.movie_detail_age);
-        rating = v.findViewById(R.id.movie_detail_rating);
+        Intent intent = getIntent();
+        this.movie = (Movie) intent.getSerializableExtra("MOVIE");
 
         Picasso.get().load(movie.getImageUrlPoster()).into(image);
         title.setText(movie.getTitle());
-        genre.setText("");
-        company.setText("");
-        summary.setText("");
-        age.setText("");
-        rating.setText("");
-
-        return v;
+        genre.setText("dd");
+        company.setText("vv");
+        summary.setText("bb");
+        age.setText("xx");
+        rating.setText("zz");
     }
 }
