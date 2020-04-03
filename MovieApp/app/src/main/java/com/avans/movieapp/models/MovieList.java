330f6c647e9a5e15780afa2e422cd5b9f9cfa7f7
AartPieterse <aartpieterse01@gmail.com>
2020-04-03 13:48:54 +0200

    Some tests

--- MovieApp/app/src/main/java/com/avans/movieapp/models/MovieList.java
+++ MovieApp/app/src/main/java/com/avans/movieapp/models/MovieList.java
@@ -17,4 +17,32 @@ public class MovieList {
     public int getId() {
         return id;
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public void setItemCount(int itemCount) {
+        this.itemCount = itemCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- MovieApp/app/src/test/java/com/avans/movieapp/ExampleUnitTest.java
+++ MovieApp/app/src/test/java/com/avans/movieapp/ExampleUnitTest.java
@@ -1,8 +1,12 @@
 package com.avans.movieapp;
 
+import com.avans.movieapp.models.MovieDetails;
+
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +14,34 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    private Date date = new Date();
+    private ArrayList<Integer> listInteger = new ArrayList<>();
+    private ArrayList<String> listString = new ArrayList<>();
+
+    private MovieDetails movie = new MovieDetails(1, "Henk, The Movie", "Overview", "", "", true, date, 4.2, listInteger, 12, "Action", "FireFox", listString, "Henk van Voort");
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void createMovieWithCapitalsResultTrue() {
+
+        Assert.assertEquals("Henk, The Movie", movie.getTitle());
+    }
+
+    @Test
+    public void createMovieNoCapitalsResultFalse() {
+
+        Assert.assertNotEquals("henk, the movie", movie.getTitle());
+    }
+
+    @Test
+    public void createMovieAdultToStringResultTrue() {
+
+        Assert.assertEquals("yes", movie.isAdultString());
+    }
+
+    @Test
+    public void createMovieAdultToStringResultFalse() {
+
+        Assert.assertNotEquals("true", movie.isAdultString());
     }
 }
